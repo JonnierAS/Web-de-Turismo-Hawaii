e4db9614027759e99fe1938e8040270ca8f815f3
Jonnier <jonniersalaverria@gmail.com>
2023-10-23 10:12:47 -0500

    traslado de proyecto guardado a github

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Web-de-Turismo-Hawaii
+
+Primeras web de practicas
+
+<img src="./imagenes/hawai.png" >
---
+++ script.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper(".mySwiper",{
+    effect: "coverflow",
+    grabCoursor: true,
+    conteredSolides: true,
+    slidesPerView: "auto",
+    coverflowEffect:{
+        rotate:15,
+        strech:0,
+        depth:300,
+        modifer:1,
+        slideShadows:true,
+    },
+    loop:true,
+});
+
+
